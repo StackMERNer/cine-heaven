4475916e57f229c99c743ea95b4353aa5d9e0c45
Md Ashraful Islam <ms785314@gmail.com>
2024-06-17 00:29:58 +0600

    update ToopRatedMovies.tsx

--- src/components/TopRatedMovies.tsx
+++ src/components/TopRatedMovies.tsx
@@ -1,7 +1,8 @@
 import clsx from "clsx";
-import { Movie } from "../hooks/usePopularMovies";
+
 import { IoStar } from "react-icons/io5";
 import { Link } from "react-router-dom";
+import { Movie } from "../hooks/useMovies";
 const TopRatedMovieList = ({
   movies,
   className,
@@ -14,7 +15,11 @@ const TopRatedMovieList = ({
       <h1 className="text-brand py-1 mb-3 text-xl font-bold">Top Rated</h1>
       <div className="flex  flex-row items-center md:overflow-auto overflow-x-scroll px-3">
         {movies?.map((movie) => (
-          <Link to={`/${movie.id}`} key={movie.id} className="flex items-center gap-2 min-w-[180px]">
+          <Link
+            to={`/movie/${movie.id}`}
+            key={movie.id}
+            className="flex items-center gap-2 min-w-[180px]"
+          >
             <img
               src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`}
               alt={movie.title}
