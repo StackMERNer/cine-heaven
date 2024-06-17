7817661f757cacbf08bc0399d0b352c42ac6874d
Md Ashraful Islam <ms785314@gmail.com>
2024-06-17 11:13:19 +0600

    update MovieCard, display movie title and formated release date

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { IoPlaySkipForward } from "react-icons/io5";
+import { IoPlaySkipForward, IoStar } from "react-icons/io5";
 import { Link } from "react-router-dom";
 import { MediaType, Movie } from "../hooks/useMovies";
 
@@ -9,7 +9,17 @@ interface MovieCardProps {
 }
 
 const MovieCard: React.FC<MovieCardProps> = ({ movie, mediaType }) => {
-  // console.log('movie',movie);
+  const formatDate = (dateString: string): string | null => {
+    if (!dateString) {
+      return null;
+    }
+    const date = new Date(dateString);
+    return date.toLocaleDateString("en-US", {
+      year: "numeric",
+      month: "short",
+      day: "numeric",
+    });
+  };
   return (
     <Link to={`/${mediaType}/${movie.id}`}>
       <div className="rounded-sm w-full  shadow-xl cursor-pointer">
@@ -22,8 +32,19 @@ const MovieCard: React.FC<MovieCardProps> = ({ movie, mediaType }) => {
           <div className="h-full w-full absolute bg-gray-400  top-0 opacity-50 group-hover:flex justify-center items-center hidden transition-all duration-1000">
             <IoPlaySkipForward size={50} />
           </div>
+          <p className="flex items-center justify-center gap-2  bg-dark-primary/[.8] text-yellow-400 px-1 absolute right-0 bottom-0 ">
+            <IoStar />
+            <span>{movie.vote_average.toFixed(1)}</span>
+          </p>
         </figure>
-        <div className="card-body"></div>
+        <div className="card-body p-1">
+          <h1 className="text-nowrap overflow-ellipsis overflow-hidden font-semibold">
+            {movie.title}
+          </h1>
+          <p className="text-xs text-gray-400 font-semibold">
+            {formatDate(movie.release_date)}
+          </p>
+        </div>
       </div>
     </Link>
   );
